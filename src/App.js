779dcdc08cf6ed81b085c133897cb7d02964622b
bossmim <49406635+bossmim@users.noreply.github.com>
2022-06-03 11:57:42 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 
 function App() {
   const [categories, setCategories] = useState([]);
-  const [filter, setFilter] = useState();
-  const display = categories.filter((categorie) => categorie.toLowerCase().includes(filter));
+  const [filter, setFilter] = useState("");
+  const display = categories.filter((categorie) =>
+    categorie.toLowerCase().includes(filter)
+  );
 
   async function fetchData() {
     let result = 0;
@@ -25,21 +27,23 @@ function App() {
   return (
     <div className="App">
       <div className="Container">
-      <span>Filter: </span>
-      <input
-        type="text"
-        defaultValue={filter}
-        onChange={(e) => setFilter(e.target.value)}
-      />
+        <span>Filter: </span>
+        <input
+          type="text"
+          defaultValue={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
       </div>
       <table>
-        {display.map((categorie) => {
-          return (
-            <tr>
-              <td>{categorie}</td>
-            </tr>
-          );
-        })}
+        <tbody>
+          {display.map((categorie, index) => {
+            return (
+              <tr key={`${categorie}-${index}`}>
+                <td>{categorie}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
