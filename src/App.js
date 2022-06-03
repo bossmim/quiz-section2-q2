64776b563063daa6d045601ccae766ae0271c1a4
bossmim <49406635+bossmim@users.noreply.github.com>
2022-06-03 11:43:49 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [categories, setCategories] = useState([]);
+  const [filter, setFilter] = useState();
+  const display = categories.filter((categorie) => categorie.toLowerCase().includes(filter));
+
+  async function fetchData() {
+    let result = 0;
+
+    await fetch("https://api.publicapis.org/categories")
+      .then((res) => res.json())
+      .then((response) => {
+        result = response.categories;
+      });
+
+    setCategories(result);
+    return result;
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="Container">
+      <span>Filter: </span>
+      <input
+        type="text"
+        defaultValue={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      </div>
+      <table>
+        {display.map((categorie) => {
+          return (
+            <tr>
+              <td>{categorie}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+}
+
+export default App;
